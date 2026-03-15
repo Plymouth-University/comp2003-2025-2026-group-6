c4dba465c37b8117053d1f2cc75424816463e81e
RjuuShrestha <aarjuushrestha123@gmail.com>
2026-03-15 19:59:47 +0000

    clean up config and init

--- UI-Design/UI prototype/firebase.js
+++ UI-Design/UI prototype/firebase.js
@@ -1,18 +1,20 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYQeZzdYUqKYiiwcpFp8BNZQsJNHtoMXo",
-  authDomain: "ctf-game-06.firebaseapp.com",
-  projectId: "ctf-game-06",
-  storageBucket: "ctf-game-06.firebasestorage.app",
+  apiKey:            "AIzaSyAYQeZzdYUqKYiiwcpFp8BNZQsJNHtoMXo",
+  authDomain:        "ctf-game-06.firebaseapp.com",
+  projectId:         "ctf-game-06",
+  storageBucket:     "ctf-game-06.firebasestorage.app",
   messagingSenderId: "509318018925",
-  appId: "1:509318018925:web:a91ddc9f7fc130dfe54c64",
-  measurementId: "G-L4WP0E0V6S"
+  appId:             "1:509318018925:web:a91ddc9f7fc130dfe54c64",
+  measurementId:     "G-L4WP0E0V6S"
 };
 
-const app = initializeApp(firebaseConfig);
+const app  = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db   = getFirestore(app);
 
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut };
+export { app, auth, db };
